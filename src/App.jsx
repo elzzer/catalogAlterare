78ecb49b20573ece33e1726fd4acf785bc57c115
elzzer <ferdinangutana@gmail.com>
2022-05-11 08:28:22 +0700

    halaman home n design fix done

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ import btnWhatsapp from "./images/btnWhatsapp.png";
 
 const App = () => {
   return (
-    <div style={{backgroundImage:`url(${backgroundImage})`, backgroundSize:"cover", width:"100%", height:"1000px"}}>
-      <img src={logoImage} alt="" style={{position:"relative", width:"300px", top:"25%", display:"block", marginLeft:"auto", marginRight:"auto"}}/>
-      <button style={{border:"none", background:`url(${btnShopee})`, backgroundSize:"cover", width:"56%", height:"3.5%", position:"relative", top:"30%", display:"block", marginLeft:"auto", marginRight:"auto"}}></button>
-      <button style={{border:"none", background:`url(${btnWhatsapp})`, backgroundSize:"cover", width:"56%", height:"3.5%", position:"relative", top:"32%", display:"block", marginLeft:"auto", marginRight:"auto"}}></button>
-      <button style={{border:"none", background:`url(${btnCatalogue})`, backgroundSize:"cover", width:"56%", height:"3.5%", position:"relative", top:"34%", display:"block", marginLeft:"auto", marginRight:"auto"}}></button>
+    <div style={{backgroundImage:`url(${backgroundImage})`, backgroundSize:"cover", width:"100%", height:"900px"}}>
+      <img src={logoImage} alt="" style={{position:"relative", width:"280px", top:"30%", display:"block", marginLeft:"auto", marginRight:"auto"}}/>
+      <button style={{border:"none", background:`url(${btnShopee})`, backgroundSize:"cover", width:"51%", height:"3.5%", position:"relative", top:"33.5%", display:"block", marginLeft:"auto", marginRight:"auto"}}></button>
+      <button style={{border:"none", background:`url(${btnWhatsapp})`, backgroundSize:"cover", width:"51%", height:"3.5%", position:"relative", top:"35%", display:"block", marginLeft:"auto", marginRight:"auto"}}></button>
+      <button style={{border:"none", background:`url(${btnCatalogue})`, backgroundSize:"cover", width:"51%", height:"3.5%", position:"relative", top:"36.5%", display:"block", marginLeft:"auto", marginRight:"auto"}}></button>
     </div>
   );
 }
