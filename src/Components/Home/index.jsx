7cfb3d64baa5b49df4d5e1a36e6f89fd6b61dcdf
elzzer <ferdinangutana@gmail.com>
2022-05-25 13:53:15 +0700

    popup and center format done

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -5,37 +5,39 @@ import './index.css'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";   
 import logo from '../../images/logoAlterare.png';
+import { Modal } from 'react-bootstrap';
 
 import PopupProduct from '../PopupProduct';
 const Home = () => {
     const [asd, setAsd] = useState();
     const [show, setShow] = useState(false);
+    const [imageShow, setImageShow] = useState("");
     const [listImage, setListImage] = useState([]);
     const responsive = 
         {
             desktop: {
-            breakpoint: {
-                max: 3000,
-                min: 1024
-            },
-            items: 1,
-            partialVisibilityGutter: 10
+                breakpoint: {
+                    max: 3000,
+                    min: 1024
+                },
+                items: 1,
+                partialVisibilityGutter: 20
             },
             mobile: {
-            breakpoint: {
-                max: 464,
-                min: 0
-            },
-            items: 1,
-            partialVisibilityGutter: 30
+                breakpoint: {
+                    max: 464,
+                    min: 0
+                },
+                items: 1,
+                partialVisibilityGutter: 30
             },
             tablet: {
-            breakpoint: {
-                max: 1024,
-                min: 464
-            },
-            items: 2,
-            partialVisibilityGutter: 30
+                breakpoint: {
+                    max: 1024,
+                    min: 464
+                },
+                items: 1,
+                partialVisibilityGutter: 30
             }
         };
     const images = [
@@ -49,8 +51,7 @@ const Home = () => {
         "https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
         "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
         "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-        "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-        "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
     ];
     
     function onChangeCarousel(e){
@@ -79,15 +80,15 @@ const Home = () => {
             return (
                 <>
                     <div style={{width:"5vw", float:"left"}}>&nbsp;</div>
-                    <img src={asd[image]} style={{float:"left", width:"28vw", height:"10vh", borderRadius:"10%", margin:"1%"}}/>
+                    <img src={asd[image]} style={{float:"left", width:"28vw", height:"10vh", borderRadius:"10%", margin:"1%"}} onClick={() => onClickImageZoom(image)}/>
                 </>
             );
         else if((index+1) % 3 == 2)
-            return <img src={asd[image]} style={{float:"left", width:"28vw", height:"10vh", borderRadius:"10%", margin:"1%"}}/>;
+            return <img src={asd[image]} style={{float:"left", width:"28vw", height:"10vh", borderRadius:"10%", margin:"1%"}} onClick={() => onClickImageZoom(image)}/>;
         else {
             return (
                 <>
-                    <img src={asd[image]} style={{float:"left", width:"28vw", height:"10vh", borderRadius:"10%", margin:"1%"}}/>
+                    <img src={asd[image]} style={{float:"left", width:"28vw", height:"10vh", borderRadius:"10%", margin:"1%"}} onClick={() => onClickImageZoom(image)}/>
                     <div style={{width:"5vw", float:"left"}}>&nbsp;</div>
                     <br style={{clear:"both"}} />
                 </>
@@ -96,18 +97,36 @@ const Home = () => {
         }
     }
 
+    function onClickImageZoom(image){
+        alert(image);
+        setShow(true);
+        setImageShow(image);
+    }
+
+    function renderImageShow(){
+        return(
+            <>
+                <Modal style={{ fontSize: '15pt', opacity:1, backgroundColor:"transparent" }}
+                    show={ show } 
+                    onHide={ handleClose }>
+                        <div style={{position: "fixed",top:"50vh", transform: "translate(0, -50%)", width:"95vw"}}>
+                            <center>
+                                <img src={asd[imageShow]} style={{width:"95vw", justifyContent:"center"}}/>
+                            </center>
+                        </div>
+                </Modal>
+            </>
+        );
+    }
+
+    function handleClose() { 
+        setShow(false);
+    }
+
     return(
-        <div style={{backgroundColor:"rgb(242,242,242)"}}>
+        <div style={{height:"100%", backgroundColor:"rgb(242,242,242)"}}>
             <center><img src={logo} style={{width:"40vw", margin:"7% 0 7% 0"}}/></center>
-            {
-                // images.map((image, id) => {
-                //     return (
-                //         <div key={id} className="item">
-                //             <img src={image}/>
-                //         </div>
-                //     )
-                // })
-            }
+
             <Carousel
                 arrows={false}
                 autoPlay={false}
@@ -122,14 +141,13 @@ const Home = () => {
                 swipeable={true}
                 responsive={responsive}
                 afterChange={(beforeSlide, { currentSlide, onMove }) => {
-                    // alert("curr : " + currentSlide + "\nbefore : " + beforeSlide);
-                    setShow(true);
+                    alert("curr : " + currentSlide + "\nbefore : " + beforeSlide);
                 }}
                 >
                 {images.map((image, id) => {
                     return (
-                        <div key={id} className="item">
-                            <img src={image}/>
+                        <div key={id} className='item'>
+                            <img src={image} style={{marginLeft:"2vw"}}/>
                         </div>
                     )
                 })}
@@ -144,7 +162,9 @@ const Home = () => {
                     )
                 })
             }
+            <br style={{clear:"both"}} />
             {/* {show ? <PopupProduct show={show} setShow={setShow} /> : ""} */}
+            {show ? renderImageShow() : ""}
         </div>
     );
 }
